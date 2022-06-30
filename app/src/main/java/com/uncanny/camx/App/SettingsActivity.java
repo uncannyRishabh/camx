58a00bc31ec53f047229b6675786485347a3f557
uncannyRishabh <rishabhrajgupta2000@gmail.com>
2022-06-30 22:04:42 +0530

    - Fixed Material You design implementation in SettingsActivity
    Signed-off-by: uncannyRishabh <rishabhrajgupta2000@gmail.com>

--- app/src/main/java/com/uncanny/camx/App/SettingsActivity.java
+++ app/src/main/java/com/uncanny/camx/App/SettingsActivity.java
@@ -1,34 +1,30 @@
 package com.uncanny.camx.App;
 
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.ContextThemeWrapper;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.WindowCompat;
 
-import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.uncanny.camx.R;
 
 import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
     private static final String TAG = "SettingsActivity";
-    private String c2status,c2text;
+    private int DEVICE_ACCENT;
+    private String c2status;
 
     private CollapsingToolbarLayout collapsingToolbarLayout;
-    private RelativeLayout s_description;
-    private Toolbar toolbar;
-    private AppBarLayout appBarLayout;
-    private TextView camera2status,camera2text;
-
-    private int DEVICE_ACCENT;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,37 +33,36 @@ public class SettingsActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(toolbar);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-
-        DEVICE_ACCENT = getDeviceAccent();
-
-        getWindow().setStatusBarColor(Color.parseColor("#141415"));
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        DEVICE_ACCENT = getDeviceAccent();
         c2status = getIntent().getStringExtra("c2api");
-
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        appBarLayout = findViewById(R.id.appbar_layout);
 
         collapsingToolbarLayout = findViewById(R.id.collapsingToolBar);
-        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
-        collapsingToolbarLayout.setExpandedTitleColor(DEVICE_ACCENT); //TODO : ADD CONDITIONS FOR DYNAMIC THEMING
+        if(Build.VERSION.SDK_INT<=Build.VERSION_CODES.S)
+            collapsingToolbarLayout.setExpandedTitleColor(DEVICE_ACCENT); //TODO : ADD CONDITIONS FOR DYNAMIC THEMING
 
-        camera2status = findViewById(R.id.c2api_check_status);
-        c2status =  camera2status.getText() + c2status;
-        camera2status.setText(c2status);
+        RelativeLayout s0 = findViewById(R.id.s_c0);
+        setDescriptionFromIncludeId(s0,"Camera 2API status", c2status);
 
-        camera2text = findViewById(R.id.c2api_check_text);
-        c2text = "\n\u25CF LEGACY: These devices expose capabilities to apps through the Camera API2 interfaces that are approximately the same capabilities as those exposed to apps through the Camera API1 interfaces. The legacy frameworks code conceptually translates Camera API2 calls into Camera API1 calls; legacy devices don't support Camera API2 features such as per-frame controls.\n\n" +
-                 "\u25CF LIMITED: These devices support some Camera API2 capabilities (but not all) and must use Camera HAL 3.2 or higher.\n\n" +
-                 "\u25CF FULL: These devices support all of the major capabilities of Camera API2 and must use Camera HAL 3.2 or higher and Android 5.0 or higher.\n\n" +
-                 "\u25CF LEVEL_3: These devices support YUV reprocessing and RAW image capture, along with additional output stream configurations.\n\n" +
-                 "\u25CF EXTERNAL: These devices are similar to LIMITED devices with some exceptions; for example, some sensor or lens information may not be reported or have less stable frame rates. This level is used for external cameras such as USB webcams.\n\n";
-        camera2text.setText(c2text);
+        String c2text = "\n\u25CF LEGACY: These devices expose capabilities to apps through the Camera API2 interfaces that are approximately the same capabilities as those exposed to apps through the Camera API1 interfaces. The legacy frameworks code conceptually translates Camera API2 calls into Camera API1 calls; legacy devices don't support Camera API2 features such as per-frame controls.\n\n" +
+                "\u25CF LIMITED: These devices support some Camera API2 capabilities (but not all) and must use Camera HAL 3.2 or higher.\n\n" +
+                "\u25CF FULL: These devices support all of the major capabilities of Camera API2 and must use Camera HAL 3.2 or higher and Android 5.0 or higher.\n\n" +
+                "\u25CF LEVEL_3: These devices support YUV reprocessing and RAW image capture, along with additional output stream configurations.\n\n" +
+                "\u25CF EXTERNAL: These devices are similar to LIMITED devices with some exceptions; for example, some sensor or lens information may not be reported or have less stable frame rates. This level is used for external cameras such as USB webcams.\n\n";
+    }
 
+    private void setDescriptionFromIncludeId(View v, String headerText, String descriptionText){
+        TextView settingHeader = v.findViewById(R.id.s_heading);
+        TextView settingDescription = v.findViewById(R.id.s_description);
+
+        settingHeader.setText(headerText);
+        settingDescription.setText(descriptionText);
     }
 
     private int getDeviceAccent(){
